6848274f3b2fd677bf8742d968847d5e60f6a0a0
Zixin Zou <zxinmm@gmail.com>
2022-01-09 13:38:47 -0500

    bonus added

--- Bonus/script.js
+++ Bonus/script.js
@@ -1,1 +1,8 @@
 
+document.querySelector("button").addEventListener("click",function(e){
+    e.preventDefault();
+    
+    const radius = document.getElementById("radius").value
+
+    document.getElementById("volume").textContent= "Sphere radius: "+radius+" Volume: "+(4/3 * Math.PI * Math.pow(Number(radius),3))
+})
